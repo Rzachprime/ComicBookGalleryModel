5653b9b26b1891c372339c0003ba7655a081526d
Richard Z Troester <rzachprime@gmail.com>
2017-08-01 21:55:05 -0400

    Updated the EF design and added a context class

--- ComicBookGalleryModel/Program.cs
+++ ComicBookGalleryModel/Program.cs
@@ -10,6 +10,23 @@ namespace ComicBookGalleryModel
     {
         static void Main(string[] args)
         {
+            using (var context = new Context())
+            {
+                context.ComicBooks.Add(new Models.ComicBook()
+                {
+                    SeriesTitle = "The Amazing Spider-Man",
+                    IssueNumber = 1,
+                    PublishedOn = DateTime.Today
+                });
+                context.SaveChanges();
+
+                var comicBooks = context.ComicBooks.ToList();
+                foreach (var comicBook in comicBooks)
+                {
+                    Console.WriteLine(comicBook.SeriesTitle);
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
